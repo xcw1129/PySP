1e2ebe2ffdac8fe1c0703bf5ff19b65b1f1d1d17
xcw <xcw1824@outlook.com>
2024-11-19 17:32:04 +0800

    test

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ TEST_DATA["data1"] = DE
 # 测试设置
 Data = TEST_DATA["data1"]
 Fs = 12000
-SAVEFIG = False
+SAVEFIG = True
 
 IF_TEST_PLOT_SPECTRUM = True
 IF_TEST_PLOT_SPECTROGRAM = True
@@ -45,10 +45,12 @@ IF_TEST_BASICSP_WINDOW = True
 IF_TEST_BASICSP_FT = True
 IF_TEST_BASICSP_STFT = False
 IF_TEST_BASICSP_ISTFT = False
-IF_TEST_CEP_CEPREAL = False
-IF_TEST_CEP_CEPPOWER = False
-IF_TEST_CEP_CEPCOMPLEX = False
-IF_TEST_CEP_CEPANALYTIC = False
+IF_TEST_CEP_CEPREAL = True
+IF_TEST_CEP_CEPPOWER = True
+IF_TEST_CEP_CEPCOMPLEX = True
+IF_TEST_CEP_CEPRECONSTRUCT = True
+IF_TEST_CEP_CEPANALYTIC = True
+IF_TEST_CEP_CEPZOOM = True
 # --------------------------------------------------------------------------------------------#
 # 开始测试并记录
 log_file = os.path.join(BASE_DIR + "//test", "test_log.txt")
@@ -128,7 +130,7 @@ with open(log_file, "w", encoding="utf-8") as f:
     # ---------------------------------------------------------------------------------------#
     if IF_TEST_SIGNAL_RESAMPLE:
         try:
-            res = Signal.resample(Sig=Sig_test,down_fs=5000, t0=0.1, T=0.5).plot(
+            res = Signal.resample(Sig=Sig_test, down_fs=5000, t0=0.1, T=0.5).plot(
                 title="Signal.resample()", savefig=SAVEFIG
             )
             print("\tSignal.resample()测试通过")
@@ -217,9 +219,10 @@ with open(log_file, "w", encoding="utf-8") as f:
     if IF_TEST_CEP_CEPREAL:
         try:
             res = Cep_Analysis.Cep_Analysis(
-                Sig_test,
+                Sig=Sig_test,
                 plot=True,
                 plot_save=SAVEFIG,
+                xlabel="倒频率q/s",
                 plot_lineinterval=0.1,
                 ylim=(-1, 1),
                 title="Cep_Analysis.Cep_Analysis().Cep_Real()",
@@ -233,9 +236,10 @@ with open(log_file, "w", encoding="utf-8") as f:
     if IF_TEST_CEP_CEPPOWER:
         try:
             res = Cep_Analysis.Cep_Analysis(
-                Sig_test,
+                Sig=Sig_test,
                 plot=True,
                 plot_save=SAVEFIG,
+                xlabel="倒频率q/s",
                 title="Cep_Analysis.Cep_Analysis().Cep_Power()",
             ).Cep_Power()
             print("\tCep_Analysis.Cep_Analysis().Cep_Power()测试通过")
@@ -247,9 +251,10 @@ with open(log_file, "w", encoding="utf-8") as f:
     if IF_TEST_CEP_CEPCOMPLEX:
         try:
             res = Cep_Analysis.Cep_Analysis(
-                Sig_test,
+                Sig=Sig_test,
                 plot=True,
                 plot_save=SAVEFIG,
+                xlabel="倒频率q/s",
                 title="Cep_Analysis.Cep_Analysis().Cep_Complex()",
             ).Cep_Complex()
             print("\tCep_Analysis.Cep_Analysis().Cep_Complex()测试通过")
@@ -258,12 +263,32 @@ with open(log_file, "w", encoding="utf-8") as f:
             print("\tCep_Analysis.Cep_Analysis().Cep_Complex()测试失败:", e)
             f.write(f"\tCep_Analysis.Cep_Analysis().Cep_Complex(): {e}\n")
     # ---------------------------------------------------------------------------------------#
+    if IF_TEST_CEP_CEPRECONSTRUCT:
+        try:
+            q_Axis, complex_cep = Cep_Analysis.Cep_Analysis(
+                Sig=Sig_test,
+            ).Cep_Complex()
+            res = Cep_Analysis.Cep_Analysis.Cep_Reconstruct(
+                q_Axis=q_Axis,
+                data=complex_cep,
+                plot=True,
+                savefig=SAVEFIG,
+                xlabel="时间t/s",
+                title="Cep_Analysis.Cep_Analysis.Cep_Reconstruct()",
+            )
+            print("\tCep_Analysis.Cep_Analysis.Cep_Reconstruct()测试通过")
+            f.write("\tCep_Analysis.Cep_Analysis.Cep_Reconstruct()测试通过\n")
+        except Exception as e:
+            print("\tCep_Analysis.Cep_Analysis.Cep_Reconstruct()测试失败:", e)
+            f.write(f"\tCep_Analysis.Cep_Analysis.Cep_Reconstruct(): {e}\n")
+    # ---------------------------------------------------------------------------------------#
     if IF_TEST_CEP_CEPANALYTIC:
         try:
             res = Cep_Analysis.Cep_Analysis(
                 Sig_test,
                 plot=True,
                 plot_save=SAVEFIG,
+                xlabel="倒频率q/s",
                 title="Cep_Analysis.Cep_Analysis().Cep_Analytic()",
             ).Cep_Analytic()
             print("\tCep_Analysis.Cep_Analysis().Cep_Analytic()测试通过")
@@ -274,7 +299,21 @@ with open(log_file, "w", encoding="utf-8") as f:
 
     print("Cep_Analysis.py测试完成\n\n")
     f.write("Cep_Analysis.py测试完成\n\n")
-
+    # ---------------------------------------------------------------------------------------#
+    if IF_TEST_CEP_CEPZOOM:
+        try:
+            res=Cep_Analysis.Cep_Analysis(
+                Sig=Sig_test,
+                plot=True,
+                plot_save=SAVEFIG,
+                xlabel="倒频率q/s",
+                title="Cep_Analysis.Cep_Analysis().Cep_Zoom()",
+            ).Cep_Zoom(fc=1000,bw=200)
+            print("\tCep_Analysis.Cep_Analysis().Cep_Zoom()测试通过")
+            f.write("\tCep_Analysis.Cep_Analysis().Cep_Zoom()测试通过\n")
+        except Exception as e:
+            print("\tCep_Analysis.Cep_Analysis().Cep_Zoom()测试失败:", e)
+            f.write(f"\tCep_Analysis.Cep_Analysis().Cep_Zoom(): {e}\n")   
 # --------------------------------------------------------------------------------------------#
 # 测试完成
 print("测试日志已保存到.../test/test_log.txt")
