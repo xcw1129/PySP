35c58e268e2af06b02550328b9d174b6cbc3b89c
xcw <xcw1824@outlook.com>
2025-04-14 11:36:52 +0800

    简化Signal的运算支持函数

--- PySP/Signal.py
+++ PySP/Signal.py
@@ -1,21 +1,18 @@
 """
 # Signal
-PySP库的框架模块, 定义了其他模块使用的基础类, 以及一些信号预处理函数
+信号数据模块, 定义了PySP库中的信号对象的基本结构, 以及一些信号预处理函数
 
 ## 内容
     - class:
-        1. Signal: 自带采样信息的信号类, 可进行简单预处理操作
-        2. Analysis: 信号分析基类, 用于创建其他复杂的信号分析、处理方法
+        1. Signal: 自带采样信息的信号类, 支持print、len、运算、数组切片和numpy函数调用
     - function:
         1. resample: 对信号进行任意时间段的重采样
+        2. Periodic: 生成仿真含噪准周期信号
 """
 
-from .dependencies import Optional, Union
+from .dependencies import Optional
 from .dependencies import np, random
-from .dependencies import inspect
 from .dependencies import copy
-from .dependencies import wraps
-from .dependencies import get_origin, get_args
 from .decorators import Input
 
 from .Plot import LinePlot
@@ -27,7 +24,7 @@ from .Plot import LinePlot
 # ---------## --------------------------------------------------------------------------------#
 class Signal:
     """
-    自带采样信息的信号类, 可进行简单预处理操作
+    自带采样信息的信号类, 支持print、len、运算、数组切片和numpy函数调用
 
     参数:
     --------
@@ -256,33 +253,7 @@ class Signal:
         """
         实现Signal对象与Signal/array/标量对象的减法运算
         """
-        if isinstance(other, Signal):
-            if self.fs != other.fs or self.N != other.N or self.t0 != other.t0:
-                raise ValueError("两个信号的采样参数不一致, 无法运算")
-            return Signal(
-                self.data - other.data,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        elif isinstance(other, np.ndarray):
-            if other.ndim != 1 or len(other) != self.N:
-                raise ValueError("数组维度或长度与信号不匹配, 无法运算")
-            return Signal(
-                self.data - other,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        elif np.isscalar(other):  # 检查是否为标量
-            return Signal(
-                self.data - other,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        else:
-            raise TypeError(f"不支持Signal对象与{type(other).__name__}类型进行运算操作")
+        return self.__add__(-other)
 
     # ----------------------------------------------------------------------------------------#
     def __mul__(self, other):
@@ -322,33 +293,7 @@ class Signal:
         """
         实现Signal对象与Signal/array/标量对象的除法运算
         """
-        if isinstance(other, Signal):
-            if self.fs != other.fs or self.N != other.N or self.t0 != other.t0:
-                raise ValueError("两个信号的采样参数不一致, 无法运算")
-            return Signal(
-                self.data / other.data,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        elif isinstance(other, np.ndarray):
-            if other.ndim != 1 or len(other) != self.N:
-                raise ValueError("数组维度或长度与信号不匹配, 无法运算")
-            return Signal(
-                self.data / other,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        elif np.isscalar(other):  # 检查是否为标量
-            return Signal(
-                self.data / other,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        else:
-            raise TypeError(f"不支持Signal对象与{type(other).__name__}类型进行运算操作")
+        return self.__mul__(1 / other)
 
     # ----------------------------------------------------------------------------------------#
     def __radd__(self, other):
@@ -362,26 +307,7 @@ class Signal:
         """
         实现Signal对象与Signal/array/标量对象的右减法运算
         """
-        if isinstance(other, Signal):
-            return other.__sub__(self)
-        elif isinstance(other, np.ndarray):
-            if other.ndim != 1 or len(other) != self.N:
-                raise ValueError("数组维度或长度与信号不匹配, 无法运算")
-            return Signal(
-                other - self.data,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        elif np.isscalar(other):  # 检查是否为标量
-            return Signal(
-                other - self.data,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        else:
-            raise TypeError(f"不支持Signal对象与{type(other).__name__}类型进行运算操作")
+        return -self.__sub__(other)
 
     # ----------------------------------------------------------------------------------------#
     def __rmul__(self, other):
@@ -395,26 +321,7 @@ class Signal:
         """
         实现Signal对象与Signal/array/标量对象的右除法运算
         """
-        if isinstance(other, Signal):
-            return other.__truediv__(self)
-        elif isinstance(other, np.ndarray):
-            if other.ndim != 1 or len(other) != self.N:
-                raise ValueError("数组维度或长度与信号不匹配, 无法运算")
-            return Signal(
-                other / self.data,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        elif np.isscalar(other):  # 检查是否为标量
-            return Signal(
-                other / self.data,
-                fs=self.fs,
-                t0=self.t0,
-                label=self.label,
-            )
-        else:
-            raise TypeError(f"不支持Signal对象与{type(other).__name__}类型进行运算操作")
+        return 1/self.__truediv__(other)
 
     # ----------------------------------------------------------------------------------------#
     def copy(self):
@@ -548,183 +455,3 @@ def Periodic(fs: int, T: float, CosParams: tuple, noise: float = 0) -> Signal:
     data += random.randn(len(t_Axis)) * noise  # 加入高斯白噪声
     Sig = Signal(data, fs=fs, label="仿真含噪准周期信号")
     return Sig
-
-
-# --------------------------------------------------------------------------------------------#
-class Analysis:
-    """
-    信号分析处理方法类, 用于创建其他复杂的分析处理方法
-
-    参数:
-    --------
-    Sig : Signal
-        输入信号
-    plot : bool, 默认为False
-        是否绘制分析结果图
-    plot_save : bool, 默认为False
-        是否保存绘图
-
-    属性：
-    --------
-    Sig : Signal
-        输入信号
-    plot : bool
-        是否绘制分析结果图
-    plot_save : bool
-        是否保存绘图
-    plot_kwargs : dict
-        绘图参数
-
-    方法：
-    --------
-    Plot(plot_func)
-        绘图装饰器, 用于对分析方法进行绘图
-    Input(*var_checks)
-        输入变量检查装饰器, 用于对分析方法输入变量进行检查
-    """
-
-    # ----------------------------------------------------------------------------------------#
-    def __init__(
-        self, Sig: Signal, plot: bool = False, plot_save: bool = False, **kwargs
-    ):
-        self.Sig = Sig.copy()  # 防止对原信号进行修改
-        # 绘图参数全局设置
-        self.plot = plot
-        self.plot_save = plot_save
-        self.plot_kwargs = kwargs
-
-    # ----------------------------------------------------------------------------------------#
-    @staticmethod
-    def Plot(plot_func: callable):
-        def plot_decorator(func):
-            def wrapper(self, *args, **kwargs):  # 针对Analysis类的方法进行装饰
-                res = func(self, *args, **kwargs)  # 针对Analysis类的方法进行装饰
-                if self.plot:  # 针对Analysis类的方法进行装饰
-                    self.plot_kwargs["plot_save"] = self.plot_save
-                    plot_func(
-                        *res, **self.plot_kwargs
-                    )  # 要求func返回结果格式符合plot_func的输入要求
-                return res
-
-            return wrapper
-
-        return plot_decorator
-
-    # ----------------------------------------------------------------------------------------#
-    @staticmethod
-    def Input(*var_checks):
-        # 根据json输入生成对应的变量检查装饰器
-        def decorator(func):
-            @wraps(func)  # 保留原函数的元信息：函数名、参数列表、注释文档、模块信息等
-            def wrapper(self, *args, **kwargs):  # 针对Analysis类的方法进行装饰
-                # ---------------------------------------------------------------------------#
-                # 获取函数输入变量
-                Vars = inspect.signature(func)
-                # 检查实际输入变量是否在函数参数中
-                if "kwargs" not in Vars.parameters:
-                    for var_name in kwargs:
-                        if var_name not in Vars.parameters:
-                            raise TypeError(
-                                f"输入变量{var_name}={kwargs[var_name]}不在函数{func.__name__}的参数列表中"
-                            )
-                bound_args = Vars.bind(
-                    self, *args, **kwargs
-                )  # 针对Analysis类的方法进行装饰
-                bound_args.apply_defaults()
-                # 获取变量的类型注解
-                annotations = func.__annotations__
-                var_checks_json = var_checks[0]
-                # ---------------------------------------------------------------------------#
-                # 按指定方式检查指定的变量
-                for var_name in var_checks_json:
-                    var_value = bound_args.arguments.get(var_name)  # 变量实际值
-                    var_type = annotations.get(var_name)  # 变量预设类型
-                    var_cond = var_checks_json[var_name]  # 变量额外检查条件
-                    # -----------------------------------------------------------------------#
-                    # 对于传值的函数参数进行类型检查
-                    if var_value is not None:
-                        # -------------------------------------------------------------------#
-                        # 处理 Optional 类型
-                        if get_origin(var_type) is Union:
-                            var_type = get_args(var_type)[0]
-                        # 处理float变量的int输入
-                        if var_type is float and isinstance(var_value, int):
-                            var_value = float(var_value)
-                        # 检查输入值类型是否为预设类型
-                        if var_type and not isinstance(var_value, var_type):
-                            raise TypeError(
-                                f"输入变量 '{var_name}' 类型不为要求的 {var_type.__name__}, 实际为 {type(var_value).__name__}"
-                            )
-                        # 针对某些变量类型进行额外检查
-                        # -------------------------------------------------------------------#
-                        # array类检查
-                        if isinstance(var_value, np.ndarray):
-                            # 条件1：数组维度检查
-                            if "ndim" in var_cond:
-                                if var_value.ndim != var_cond["ndim"]:
-                                    raise ValueError(
-                                        f"输入array数组 '{var_name}' 维度不为要求的 {var_cond['ndim']}, 实际为{var_value.ndim}"
-                                    )
-                        # -------------------------------------------------------------------#
-                        # int类
-                        if isinstance(var_value, int):
-                            # 条件1：下界检查
-                            if "Low" in var_cond:
-                                if not (var_cond["Low"] <= var_value):
-                                    raise ValueError(
-                                        f"输入int变量 '{var_name}' 小于要求的下界 {var_cond["Low"]}, 实际为{var_value}"
-                                    )
-                            # 条件2：上界检查
-                            if "High" in var_cond:
-                                if not (var_value <= var_cond["High"]):
-                                    raise ValueError(
-                                        f"输入int变量 '{var_name}' 大于要求的上界 {var_cond["High"]}, 实际为{var_value}"
-                                    )
-                        # -------------------------------------------------------------------#
-                        # float类
-                        if isinstance(var_value, float):
-                            # 条件1：闭下界检查
-                            if "CloseLow" in var_cond:
-                                if not (var_cond["CloseLow"] <= var_value):
-                                    raise ValueError(
-                                        f"输入float变量 '{var_name}' 小于要求的下界 {var_cond["CloseLow"]}, 实际为{var_value}"
-                                    )
-                            # 条件2：闭上界检查
-                            if "CloseHigh" in var_cond:
-                                if not (var_value <= var_cond["CloseHigh"]):
-                                    raise ValueError(
-                                        f"输入float变量 '{var_name}' 大于要求的上界 {var_cond["CloseHigh"]}, 实际为{var_value}"
-                                    )
-                            # 条件3：开下界检查
-                            if "OpenLow" in var_cond:
-                                if not (var_cond["OpenLow"] < var_value):
-                                    raise ValueError(
-                                        f"输入float变量 '{var_name}' 小于或等于要求的下界 {var_cond["OpenLow"]}, 实际为{var_value}"
-                                    )
-                            # 条件4：开上界检查
-                            if "OpenHigh" in var_cond:
-                                if not (var_value < var_cond["OpenHigh"]):
-                                    raise ValueError(
-                                        f"输入float变量 '{var_name}' 大于或等于要求的上界 {var_cond["OpenHigh"]}, 实际为{var_value}"
-                                    )
-                        # -------------------------------------------------------------------#
-                        # str类
-                        if isinstance(var_value, str):
-                            # 条件1：字符串内容检查
-                            if "Content" in var_cond:
-                                if var_value not in var_cond["Content"]:
-                                    raise ValueError(
-                                        f"输入str变量 '{var_name}' 不在要求的范围 {var_cond['Content']}, 实际为{var_value}"
-                                    )
-                        # -------------------------------------------------------------------#
-                        # Signal类
-                        if isinstance(var_value, Signal):
-                            pass
-                # ---------------------------------------------------------------------------#
-                return func(
-                    self, *args, **kwargs
-                )  # 检查通过，执行类方法# 针对Analysis类的方法进行装饰
-
-            return wrapper
-
-        return decorator
