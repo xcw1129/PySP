21ba987b57eb23ba1c3fee01d320e186f4f2de21
xcw <xcw1824@outlook.com>
2025-06-08 16:04:15 +0800

    更新版本

--- PySP/__init__.py
+++ PySP/__init__.py
@@ -2,6 +2,6 @@ from . import Signal
 from . import Analysis
 from . import Plot
 
-from .Analysis_Module import SST
+from .Analysis_Module import SST,EMD
 
-__version__ = "7.2.1"
+__version__ = "7.3.0"
