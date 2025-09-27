fc7bdc7d1d409ad60f23ba96b3b90ebe140c4c69
xcw <xcw1824@outlook.com>
2025-09-27 17:29:43 +0800

    修复

--- PySP/__init__.py
+++ PySP/__init__.py
@@ -2,6 +2,4 @@ from . import Signal
 from . import Analysis
 from . import Plot
 
-from .Analysis_Module import SST,EMD
-
-__version__ = "7.3.0"
+__version__ = "7.3.1"
--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/xcw1129/PySP",  # 项目主页
     packages=find_packages(),  # 自动发现包
-    package_data={
-        "PySP.Assist_Module": ["times+simsun.ttf"],  # 路径相对包目录
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
